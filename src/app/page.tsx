9dad987892f6a0a7defa5c09bce72982dbfe7781
MayukhKarmakar2004 <mayukhk2004@gmail.com>
2026-04-28 18:48:55 +0530

    Initial portfolio commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { PortfolioGallery } from "@/components/PortfolioGallery";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <PortfolioGallery />
+    </main>
+  );
+}
